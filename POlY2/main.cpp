c9d6d25adc83c6343c64d13070a16e8fbe36a914
Rahul7171 <rahulbagal7171@gmail.com>
2021-02-17 18:01:29 +0530

    Initial Commit

---
+++ POlY2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  POlY2
+//
+//  Created by Rahul Bagal on 17/02/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
